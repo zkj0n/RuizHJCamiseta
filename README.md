1cf7ce3041227852bf8d35e48ca4839e990f805c
zkjon <121103151+zkj0n@users.noreply.github.com>
2024-10-31 21:18:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Generador de Camisetas Personalizadas
+
+Esta aplicación permite a los usuarios diseñar una camiseta personalizada seleccionando imágenes, colores, y textos para adaptarla a su estilo.
+
+## Funcionalidades
+
+1. **Cambio de Título**: 
+   - El título en la camiseta (parte superior izquierda) se puede modificar introduciendo el texto deseado en el campo de entrada de texto en la esquina superior izquierda de la pantalla.
+   
+2. **Selección de Color de Camiseta**:
+   - Los botones de selección de color (radio buttons) permiten cambiar el color de fondo de la camiseta entre blanco y negro.
+   
+3. **Posicionamiento del Título**:
+   - Dos deslizadores permiten ajustar la posición del título en el eje horizontal y vertical dentro de la camiseta. Mueve los controles para colocar el título en la ubicación deseada.
+   
+4. **Imágenes de Personajes**:
+   - En el lado izquierdo de la pantalla, hay una lista de personajes para seleccionar.
+   - Los usuarios pueden:
+     - **Arrastrar y soltar** una imagen de personaje al centro de la camiseta.
+     - **Clicar** en la imagen de un personaje para que aparezca en el centro de la camiseta.
+   - Al seleccionar o arrastrar una imagen, el texto en la parte inferior de la camiseta cambia automáticamente para mostrar el nombre del personaje.
+
+## Instrucciones de Uso
+
+1. **Cambiar el Título**:
+   - Escriba el título deseado en el campo de texto (etiquetado como `title`). Esto se actualizará en la camiseta.
+
+2. **Cambiar Color de la Camiseta**:
+   - Seleccione el color de la camiseta usando los botones de selección (negro o blanco).
+
+3. **Ajustar Posición del Título**:
+   - Utilice los controles deslizantes para ajustar la posición del texto del título en el eje horizontal y vertical.
+
+4. **Agregar un Personaje**:
+   - Arrastre una imagen de personaje desde la barra lateral hacia el centro de la camiseta o haga clic en la imagen del personaje.
+   - El texto en la parte inferior de la camiseta se actualizará para reflejar el nombre del personaje seleccionado.
+
+## Tecnologías Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
---
+++ js/script.js
@@ -0,0 +1,92 @@
+// cambiar la camiseta
+function changeShirt() {
+    const shirt = document.getElementById('shirt');
+    const radio = document.getElementById('black').checked;
+    let shirtText = document.querySelector('.shirt-text');
+    let charTitle = document.querySelector('.char-title');
+    if (radio){ // si el radio button esta seleccionado se pondra la camiseta negra
+        shirt.src = '../img/camiseta/black.png';
+        shirtText.style.color = '#fff';
+        charTitle.style.color = '#fff';
+    } else {
+        shirt.src = '../img/camiseta/white.png';
+        shirtText.style.color = '#000';
+        charTitle.style.color = '#000';
+    }
+
+}
+
+document.getElementById('black').addEventListener('click', changeShirt);
+document.getElementById('white').addEventListener('click', changeShirt);
+
+// cambiar texto de la camiseta a partir del input
+function changeText() {
+    let shirtText = document.querySelector('.shirt-text');
+    let title = document.getElementById('title').value;
+    // si el texto supera 10 caracteres saltara un error
+    if (title.length > 10){
+        alert('el texto no puede superar los 10 caracteres');
+    } else if (title.length === 0){
+        shirtText.innerHTML = 'title'; // si no hay texto se pondra title
+    } 
+    else {
+        shirtText.innerHTML = title;
+    }
+}
+document.getElementById('title').addEventListener('input', changeText);
+
+
+// subir o bajar el texto de la camiseta
+const rangeEjeX = document.getElementById('ejeX');
+const rangeEjeY = document.getElementById('ejeY');
+
+
+function moveText() {
+    let shirtText = document.querySelector('.shirt-text');
+    // transformar para que suba o baje a partir de donde esta
+    shirtText.style.transform = `translate(${rangeEjeX.value}%, ${rangeEjeY.value}%)`;
+}
+
+rangeEjeX.addEventListener('input', moveText);
+rangeEjeY.addEventListener('input', moveText);
+let charTitle = document.querySelector('.char-title');
+
+// arrastrar las imagenes a la camiseta
+const imagenes = document.querySelectorAll('.img');
+const shirt = document.querySelector('.shirt');
+const shirtImage = document.querySelector('.shirt-image');
+const reverseImage = document.querySelector('.reverse-image');
+let nombre =""; // aqui almacenamos el nombre de la imagen que se esta moviendo
+// al comenzar a mover la imagen se guarda la ruta 
+imagenes.forEach(imagen => {
+    imagen.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('src', event.target.src);
+        nombre= event.target.id; // guardamos el nombre de la imagen
+    });
+});
+// prevenir que se abra la imagen al soltarla
+shirtImage.addEventListener('dragover', (event) => {
+    event.preventDefault();
+});
+// al soltarla cambiar la imagen
+shirtImage.addEventListener('drop', (event) => {
+    event.preventDefault();
+    let src = event.dataTransfer.getData('src');
+    shirtImage.src = src;
+    reverseImage.src = src;
+    // cambiar el titulo de la imagen
+    charTitle.innerHTML = nombre;
+    
+});
+
+// tambien se puede clicar la imagen para cambiarla
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', (event) => {
+        shirtImage.src = event.target.src;
+        reverseImage.src = event.target.src;
+        charTitle.innerHTML = event.target.id;
+    });
+});
+
+
+
